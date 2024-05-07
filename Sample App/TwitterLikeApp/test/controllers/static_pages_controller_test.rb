97ca0dad6524a03fb730ddc734bdd9cccbca942b
Puja Sharma <pujashrma151195@gmail.com>
2024-05-07 18:02:24 +0530

    Static pages completed

--- Sample App/TwitterLikeApp/test/controllers/static_pages_controller_test.rb
+++ Sample App/TwitterLikeApp/test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,39 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+     @baseText = "Ruby on Rails Tutorial Sample App"
+  end
+
+test "should get root" do
+get root_url
+assert_response :success
+assert_select "title", "Home | #{@baseText}"
+end
+
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@baseText}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@baseText}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@baseText}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@baseText}"
   end
 end
